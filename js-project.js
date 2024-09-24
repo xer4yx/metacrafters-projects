6700bfe84636186a841c6fa70922362f74ec9d8b
Angelo Bicomong <202111173@fit.edu.ph>
2024-09-24 14:53:44 +0000

    Major changes

--- js-project.js
+++ js-project.js
@@ -9,12 +9,13 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
-let currrentUserNFT = [];
+const crypto = require('crypto');
+let mintedNFTs = [];
 
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT(network, minted) {
+function mintNFT(name, network="etherium", value=0, description=undefined) {
     const availableNetwork = ["solana", "etherium", "bitcoin", "polygon"];
 
     const listedNetwork = {
@@ -31,26 +32,36 @@ function mintNFT(network, minted) {
         "polygon": "POL"
     };
 
+    const hashItem = (input) => {
+        const hash = crypto.createHash('sha256');
+        hash.update(input);
+        return hash.digest('hex').toString();
+    }
+
+    const getDateNow = () => {
+        const date = new Date();
+        const formattedDate = date.getFullYear().toString()+
+                                date.getHours().toString()+
+                                date.getMinutes().toString()+
+                                date.getSeconds().toString();
+        return formattedDate;
+    }
+
     network = network.toString().toLowerCase();
 
     if (availableNetwork.includes(network)) {
-        const existingNFT = currrentUserNFT.find(existingNetwork => existingNetwork.network === listedNetwork[network]);
+        const nftMetadata = {
+            "unique": hashItem(name.toString()+getDateNow()),
+            "name": name,
+            "description": description !== undefined ? description : '',
+            "network": listedNetwork[network],
+            "currency": listedToken[network],
+            "value": parseFloat(value)
+        };
 
-        if (existingNFT) {
-            existingNFT.minted += parseInt(minted);
-            console.log(`Added ${minted} ${existingNFT.token} to existing ${existingNFT.network} wallet.`);
-            console.log(`Current ${existingNFT.network} minted: ${existingNFT.minted} ${existingNFT.token}.\n`);
-        } else {
-            const userNFT = {
-                "token": listedToken[network],
-                "network": listedNetwork[network],
-                "minted": parseInt(minted)
-            };
+        mintedNFTs.push(nftMetadata);
 
-            currrentUserNFT.push(userNFT);
-
-            console.log(`You have successfully minted ${userNFT.network} with a value of ${userNFT.minted} ${userNFT.token}.\n`);
-        }
+        console.log(`You have successfully minted ${nftMetadata.network} with a value of ${nftMetadata.value} ${nftMetadata.token}.\n`);
     } else {
         console.log(`Token ${network} not listed in available tokens!\n`);
     }
@@ -60,51 +71,33 @@ function mintNFT(network, minted) {
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs() {
-    if (!currrentUserNFT || currrentUserNFT.length === 0) {
+    if (!mintedNFTs || mintedNFTs.length === 0) {
         console.log("No NFTs available");
         return;
     }
 
-    // Using the max and map function to get the max length of string per object properties
-    const maxTokenLength = Math.max('Token'.length, ...currrentUserNFT.map(nft => (nft.token).toString().length));
-    const maxNetworkLength = Math.max('Network'.length, ...currrentUserNFT.map(nft => (nft.network).toString().length));
-    const maxMintedLength = Math.max('Value'.length, ...currrentUserNFT.map(nft => (nft.minted).toString().length));
+    const maxIdLen = Math.max(...mintedNFTs.map(nft => (nft.unique).toString().length));
 
-    // Helper function to add spacing based on the longest length
-    const padText = (text, maxLength) => {
-        const content = text.toString(); // Ensure the value is a string
-        return content + ' '.repeat(Math.max(0, maxLength - content.length));
-    };
-
-    const header = `| ${padText('Token', maxTokenLength)} | ${padText('Network', maxNetworkLength)} | ${padText('Value', maxMintedLength)} |`;
-    const lineSeparator = '-'.repeat(header.length);
+    const lineSeparator = '-'.repeat(maxIdLen);
 
     console.log(lineSeparator);
-    console.log(header);
-    console.log(lineSeparator);
 
-    for (const nft of currrentUserNFT) {
-        const nftToken = padText(nft.token, maxTokenLength);
-        const nftNetwork = padText(nft.network, maxNetworkLength);
-        const nftMinted = padText(nft.minted, maxMintedLength);
+    for (const nft of mintedNFTs) {
+        const listTemplate = `ID: ${nft.unique}\nName: ${nft.name}\nDescription: ${nft.description}\nNetwork: ${nft.network}\nValue: ${nft.value} ${nft.currency}`;
 
-        console.log(`| ${nftToken} | ${nftNetwork} | ${nftMinted} |`);
+        console.log(listTemplate);
+        console.log(lineSeparator);
     }
-
-    console.log(lineSeparator);
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-    console.log(`You have ${parseInt(currrentUserNFT.length)} NFTs created`);
+    console.log(`You have ${parseInt(mintedNFTs.length)} NFTs in your collection`);
 }
 
 // call your functions below this line
-mintNFT("SOLANA", 12)
-mintNFT("EthErIuM", 3)
-mintNFT("Polygon", 6)
-mintNFT("bitc0in", 4)
-mintNFT("SOLANA", 2)
+mintNFT("BAYC #10", "etherium", 0.25, "Bored Ape Society Collection")
+mintNFT("BAKC #1224")
 
 listNFTs()
 
